24397a922b84168239ece4b2aa7ff66cf727e635
IrtezaIshaq <150837618+irtazahup@users.noreply.github.com>
2026-06-24 15:39:23 +0530

    deleted the erroromous code

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
+
 const app = express();
+// 🔥 FIX 1: Add this line to parse JSON bodies
+app.use(express.json()); 
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
+
 
 app.listen(3000, () => {
   console.log("Server running");
